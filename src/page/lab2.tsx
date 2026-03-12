8582e3bc5f1c59a340bad772f74da0f2b1e7da38
Doan Trong Duong <duongdoan2k6@gmail.com>
2026-03-12 15:30:57 +0700

    lab 2

--- src/page/lab2.tsx
+++ src/page/lab2.tsx
@@ -2,45 +2,68 @@ import { Table } from "antd";
 
 const StudentList = () => {
 
-  const columns = [
-    {
-        title: "ID",    
-        dataIndex: "key",
-    },
+    const columns = [
+        {
+            title: "ID",
+            dataIndex: "key",
+        },
+        {
+            title: "Name",
+            dataIndex: "name",
+        },
+        {
+            title: "Age",
+            dataIndex: "age",
+        },
+        {
+            title: "Major",
+            dataIndex: "major"
+        },
+        {
+            title: "Hình ảnh",
+            dataIndex: "image",
+            render: (img: string) => <img src={img} width="60" />
+        },
+        {
+            title: "Status",
+            dataIndex: "status",
+            render: (status: string) => (
+                <span style={{ color: status === "active" ? "green" : "red" }}>
+                    {status}
+                </span>
+            ),
+        },
+        {
+            title: "Action",
+            render: (_: any ,record:any) => (
+                <>
+                    <button>Edit</button> |
+                    <button>Delete</button>
+                </>
+            ),
+        },
+    ];
 
-    {
-      title: "Name",
-      dataIndex: "name",
-     
-    },
-    {
-      title: "Age",
-      dataIndex: "age",
-    },
-    {
-        title:"major",
-        dataIndex:"major"
-    }
-    
-  ];
+    const data = [
+        {
+            key: "1",
+            name: "Nguyen Van A",
+            age: 20,
+            major: "Web Development",
+            image: "https://i.pravatar.cc/100?img=4",
+            status: "active"
+        },
+        {
+            key: "2",
+            name: "Tran Van B",
+            age: 25,
+            major: "Digital Marketing",
+            image: "https://i.pravatar.cc/100?img=2",
+            status: "inactive"
+        },
+    ];
 
-  const data = [
-    {
-      key: "1",
-      name: "Nguyen Van A",
-      age: 20,
-      major:"Web Development",
-      
-    },
-    {
-      key: "2",
-      name: "Tran Van B",
-      age: 25,
-      major:"Digital Marketing"
-    },
-  ];
-
-  return <Table columns={columns} dataSource={data} />;
+    return <Table columns={columns} dataSource={data} pagination={{ pageSize: 3 }}/>;
 };
 
 export default StudentList;
